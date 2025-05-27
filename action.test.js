23205b0e2a2ed2f6cdba42b71495c7bb4fdd55cb
Isaac Muniz <im.isaac.muniz@gmail.com>
2025-05-27 18:15:25 -0300

    Spying with jest

--- action.test.js
+++ action.test.js
@@ -1,6 +1,15 @@
 const fetchData = require("./action");
 
-test("Mock implementation of a basic function", () => {
-  const mock = jest.fn((x) => 42 + x);
-  expect(mock(2)).toBe(44);
+test("Spying on a method of an object", () => {
+  const video = {
+    play() {
+      return true;
+    },
+  };
+
+  const spy = jest.spyOn(video, "play");
+  video.play();
+
+  expect(spy).toHaveBeenCalled();
+  spy.mockRestore();
 });
